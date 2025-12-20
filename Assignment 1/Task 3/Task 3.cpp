f0f700a42ed9c2b9c2ee89d248af7ab295ed0a42
Diana <peachyydi@gmail.com>
2025-12-20 23:56:44 +0600

    Add Assignment 1

---
+++ Assignment 1/Task 3/Task 3.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>     // для ввода и вывода
+#include <omp.h>        // подключение OpenMP
+#include <chrono>       // для измерения времени
+#include <ctime>        // для time()
+#include <cstdlib>      // для rand() и srand()
+
+using namespace std;    // чтобы не писать std:: перед cout
+
+
+int main() {        // главная функция программы
+    const int N = 1000000;           // размер массива 1000000 элементов
+    int* arr = new int[N];               // динамически выделяет массив
+    srand((unsigned)time(0));            // инициализирует генератор случайных чисел
+
+    for (int i = 0; i < N; i++) {              // цикл для заполнения массива
+        arr[i] = rand() % 100 + 1;            // здесь случайные числа от 1 до 100
+    }
+
+
+// ПОСЛЕДОВАТЕЛЬНЫЙ ПОИСК
+    auto start_sequential = chrono::high_resolution_clock::now();           // время начала поиска
+    int min_sequential = arr[0];              // считает первый элемент минимальным
+    int max_sequential = arr[0];                 // считает первый элемент максимальным
+    for (int i = 1; i < N; i++) {          // в цикле один поток проходится по массиву
+        if (arr[i] < min_sequential) min_sequential = arr[i];           // обновляет минимум
+        if (arr[i] > max_sequential) max_sequential = arr[i];            // обновляет максимум
+    }
+    auto end_sequential = chrono::high_resolution_clock::now();   // время окончания поиска
+    chrono::duration<double, milli> time_sequential = end_sequential - start_sequential; // длительность поиска
+// ПОСЛЕДОВАТЕЛЬНЫЙ ПОИСК
+
+
+// ПАРАЛЛЕЛЬНЫЙ ПОИСК
+    auto start_parallel = chrono::high_resolution_clock::now();             // время начала поиска
+    int min_parallel = arr[0];            // общий минимум для параллельной версии
+    int max_parallel = arr[0];           // общий максимум для параллельной версии
+
+#pragma omp parallel         //  запускает параллельную секцию, где код выполняется одновременно несколькими потоками
+    {   int localMin = arr[0];            // локальный минимум потока
+        int localMax = arr[0];           // локальный максимум потока
+
+#pragma omp for nowait        // делит цикл по потокам
+        for (int i = 1; i < N; i++) {          // цикл, в котором каждый поток обрабатывает часть массива
+            if (arr[i] < localMin) localMin = arr[i];       // обновляет локальный минимум
+            if (arr[i] > localMax) localMax = arr[i];           // обновляет локальный максимум
+        }
+
+#pragma omp critical         // один поток за раз обновляет общий результат
+        {   if (localMin < min_parallel) min_parallel = localMin;           // обновляет общий минимум
+            if (localMax > max_parallel) max_parallel = localMax;           // обновляет общий максимум
+        }
+    }
+    auto end_parallel = chrono::high_resolution_clock::now();   // время окончания поиска
+    chrono::duration<double, milli> time_parallel = end_parallel - start_parallel; // длительность поиска
+// ПАРАЛЛЕЛЬНЫЙ ПОИСК
+    
+
+    cout << "sequential min: " << min_sequential << endl;    // выводит последовательный min 
+    cout << "sequential max: " << max_sequential << endl;    // выводит последовательный max 
+    cout << "sequential time: " << time_sequential.count() << endl; // выводит время последовательной версии
+    cout << "parallel min: " << min_parallel << endl;      // выводит параллельный min
+    cout << "parallel max: " << max_parallel << endl;      // выводит параллельный max
+    cout << "parallel time: " << time_parallel.count() << endl;   // выводит время параллельной версии
+    cout << "OpenMP threads: " << omp_get_max_threads() << endl; // выводит количество потоков OpenMP
+
+    delete[] arr;                                    // освобождает память
+    arr = nullptr;                                   // обнуляет указатель
+
+    return 0;                                        // завершение программы
+}
---
+++ Assignment 1/Task 4/Task 4.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>     // для ввода и вывода
+#include <omp.h>        // подключение OpenMP
+#include <chrono>       // для измерения времени
+#include <ctime>        // для time()
+#include <cstdlib>      // для rand() и srand()
+
+using namespace std;    // чтобы не писать std:: перед cout
+
+
+int main() {        // главная функция программы
+    const int N = 5000000;          // размер массива 5000000 элементов
+    int* arr = new int[N];               // динамически выделяет память под массив
+    srand((unsigned)time(0));        // инициализирует генератор случайных чисел
+    for (int i = 0; i < N; i++) {         // цикл заполняет массив
+        arr[i] = rand() % 100 + 1;               // случайными числами от 1 до 100
+    }
+
+
+// ПОСЛЕДОВАТЕЛЬНЫЙ ПОДСЧЕТ
+    auto start_sequential = chrono::high_resolution_clock::now();       // время начала подсчета
+    long long sum_sequential = 0;       // сумма элементов
+    for (int i = 0; i < N; i++) {       // цикл проходится по всем элементам массива
+        sum_sequential += arr[i];            // прибавляет текущий элемент к сумме
+    }
+    double avg_sequential = (double)sum_sequential / N;           // вычисляет среднее значение
+    auto end_sequential = chrono::high_resolution_clock::now();   // время окончания подсчета
+    chrono::duration<double, milli> time_sequential = end_sequential - start_sequential; // длительность подсчёта
+// ПОСЛЕДОВАТЕЛЬНЫЙ ПОДСЧЕТ
+
+
+
+// ПАРАЛЛЕЛЬНЫЙ ПОДСЧЕТ
+    auto start_parallel = chrono::high_resolution_clock::now();          // время начала подсчета
+    long long sum_parallel = 0;          // сумма элементов
+
+#pragma omp parallel for reduction(+:sum_parallel)       // параллельный цикл, плюс редукция суммы
+    for (int i = 0; i < N; i++) {       // цикл делится между потоками
+        sum_parallel += arr[i];           // и каждый поток считает свою часть суммы
+    }
+    double avg_parallel = (double)sum_parallel / N;           // вычисляет среднее значение
+    auto end_parallel = chrono::high_resolution_clock::now();    // время окончания подсчета
+    chrono::duration<double, milli> time_parallel = end_parallel - start_parallel;    // длительность подсчета
+// ПАРАЛЛЕЛЬНЫЙ ПОДСЧЕТ
+
+
+    cout << "sequential average: " << avg_sequential << endl;       // вывод последовательного среднего
+    cout << "sequential time: " << time_sequential.count() << endl;      // вывод последовательного времени
+    cout << "parallel average: " << avg_parallel << endl;       // вывод параллельного среднего
+    cout << "parallel time: " << time_parallel.count() << endl;     // вывод параллельного времени
+    cout << "OpenMP threads: " << omp_get_max_threads() << endl;        // вывод количества потоков
+
+    delete[] arr;        // освобождает память
+    arr = nullptr;         // обнуляет указатель
+
+    return 0;     // завершение программы
+}
